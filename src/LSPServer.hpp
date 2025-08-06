6e19a9c1553c6df42c1cae2612622c677d204da9
olivia <trevorleecox@icloud.com>
2025-08-06 18:47:48 -0500

    ok ok it my not work but the baseics are there

--- src/LSPServer.hpp
+++ src/LSPServer.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include <unordered_map>
 
 class LSPServer {
 public:
@@ -8,6 +7,4 @@ public:
 private:
     void handleRequest(const std::string& json);
     void sendResponse(int id, const std::string& result);
-
-    std::unordered_map<std::string, std::string> documents; // uri → text
 };
